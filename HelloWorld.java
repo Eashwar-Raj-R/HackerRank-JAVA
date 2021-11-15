d8000c53683244ef0e5204ce35d8d442ece77a28
Eashwar <94363541+Eashwar-Raj-R@users.noreply.github.com>
2021-11-15 16:57:11 +0530

    Welcome to Java!

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld{
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
         System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
+        System.out.println("Hello, Java."); 
     }
 }
