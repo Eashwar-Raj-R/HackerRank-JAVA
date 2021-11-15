436864ca4460d0809602c5b30b74d495a5ccd68a
Eashwar <94363541+Eashwar-Raj-R@users.noreply.github.com>
2021-11-15 17:05:12 +0530

    Java End-of-file

---
+++ EndOfFile.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class EndOfFile {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner obj=new Scanner(System.in);
+        int i=0;
+            while(obj.hasNext()){
+       i++;
+       System.out.println(i + " " + obj.nextLine());}
+        
+    }
+}
