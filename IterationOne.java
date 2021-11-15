3fe2acc27179e365fe61166379e1d8a0003ef947
Eashwar <94363541+Eashwar-Raj-R@users.noreply.github.com>
2021-11-15 17:01:54 +0530

    Java Loops I

---
+++ IterationOne.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+
+
+public class IterationOne {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        int num=obj.nextInt();
+        for(int i=1;i<=10;i++){
+           // System.out.println(num X i +"="+(num*i));
+            System.out.println(num +" x "+i+" = "+(num*i));
+        }
+        
+    }
+}
+ 
